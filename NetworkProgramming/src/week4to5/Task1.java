c3bb7da49b737316250833da9abc05f5638b2d64
YiQuan <s3522101@student.rmit.edu.au>
2017-03-27 00:21:43 +1100

    update

--- NetworkProgramming/src/week4to5/Task1.java
+++ NetworkProgramming/src/week4to5/Task1.java
@@ -28,9 +28,6 @@ public class Task1 {
 		for (NetworkInterface netIf : Collections.list(nets)) {
             System.out.printf("Display name: %s\n", netIf.getDisplayName());
             System.out.printf("Name: %s\n", netIf.getName());
-            netIf.
-//            displaySubInterfaces(netIf);
-            System.out.printf("\n");
         }
 		
 	}
@@ -43,5 +40,49 @@ public class Task1 {
             System.out.printf("\tSub Interface Display name: %s\n", subIf.getDisplayName());
             System.out.printf("\tSub Interface Name: %s\n", subIf.getName());
         }
+	}
+	
+	public static void task2Server() throws Exception{
+		/*
+		 * According to notes:
+		 * - Hosts have ports, 0->65535, server listen on a port
+		 * - Multiple clients can be communicating with a server on a given port,
+		 * each client is assigned a separate socket
+		 * - Client get port n a socket on client machine when they successfully connect with a server
+		 */
+		
+		System.out.println("Server running");
+		int clientNumber = 0;
+		ServerSocket listener = new ServerSocket(2017);
+		Socket clientSocket = listener.accept();
+		
+		//reading from client
+		BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		PrintWriter out = new PrintWriter(clientSocket.getOutputStream());
+		
+		System.out.println("Connection setup with client #" + clientNumber + "at " + clientSocket);
+		out.println("Please enter a line you would like to capitalized. Enter \"X\" to end the program.");
+		
+		while (true){
+			String input = in.readLine();
+			//end cndition
+			if (input == null || input.equals("X")){
+				break;
+			}
+			
+			//capitalized the line
+			out.println(input.toUpperCase());
+			
+			//write to file
+			
+			
+			//close the socket
+			clientSocket.close();
+		}
+	}
+	
+	public static void task2Client() {
+		BufferedReader in;
+		PrintWriter out;
 	}
 }
